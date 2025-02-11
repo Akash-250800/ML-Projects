812c7a459c050c0fbb17d654290bec0e8fec80f9
Akash-250800 <akashkrishnan96@gmail.com>
2025-02-11 09:37:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='ML Project',
 version='0.0.1',
 author='Akash',
 author_email='akashkrishnan96@gmail.com',
